6ed3af5c3a73b55ed415b0ecd8020c7e76fe0963
chandan9074 <chandan15-11611@diu.edu.bd>
2021-06-30 20:12:37 +0600

    update code

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 
 function calculation(Value){
-    console.log("chandan")
-    if(document.getElementById('chandan').innerHTML==="0"){
-        document.getElementById('chandan').innerHTML = Value;
+    if(document.getElementById('screen_txt').innerHTML==="0"){
+        document.getElementById('screen_txt').innerHTML = Value;
     }
     else{
-        document.getElementById('chandan').innerHTML += Value;
+        document.getElementById('screen_txt').innerHTML += Value;
     }
 }
 
 function answer(){
-    var result = eval(document.getElementById('chandan').innerHTML)
-    document.getElementById('chandan').innerHTML = result;
+    var result = eval(document.getElementById('screen_txt').innerHTML)
+    document.getElementById('screen_txt').innerHTML = result;
 }
 
 function refresh(){
-    console.log("clear")
-    document.getElementById('chandan').innerHTML ="0";
+    document.getElementById('screen_txt').innerHTML ="0";
 }
 
 function percentage(){
-    var per = document.getElementById("chandan").innerHTML;
+    var per = document.getElementById("screen_txt").innerHTML;
     var res = parseInt(per)
     var result = res / 100;
-    document.getElementById('chandan').innerHTML = result;
+    document.getElementById('screen_txt').innerHTML = result;
 }
 
 function del(){
-    var rem = document.getElementById('chandan').innerHTML;
+    var rem = document.getElementById('screen_txt').innerHTML;
     var update = rem.slice(0, -1)
     if(update===""){
-        document.getElementById('chandan').innerHTML ="0";
+        document.getElementById('screen_txt').innerHTML ="0";
     }
     else{
-        document.getElementById('chandan').innerHTML = update;
+        document.getElementById('screen_txt').innerHTML = update;
     }
 }
